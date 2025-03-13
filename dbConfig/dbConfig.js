9ef99ca6b418d75490047f59483b745064706ae9
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-03-13 21:47:35 +0800

    added .env

--- dbConfig/dbConfig.js
+++ dbConfig/dbConfig.js
@@ -1,14 +1,23 @@
 import mongoose from "mongoose"; // Importing mongoose using ES Modules
+import dotenv from "dotenv"; // Import dotenv to load environment variables
+
+// Load environment variables from .env file
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://Ronchiko:Mybabe0814@atlascluster.rjfmjfq.mongodb.net/storage",
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    // Use MONGO_URI from .env file
+    const mongoURI = process.env.MONGO_URI;
+
+    if (!mongoURI) {
+      console.error("MongoDB URI is missing in the .env file");
+      process.exit(1); // Exit the process if the URI is not defined
+    }
+
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("MongoDB connected");
   } catch (err) {
     console.error("Error connecting to MongoDB:", err);
